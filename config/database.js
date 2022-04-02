f039ead2796f12e18786bad5ff66edc8b61b5734
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2022-04-02 15:50:10 +0000

    updated db

--- config/database.js
+++ config/database.js
@@ -1,19 +1,32 @@
-const { parse } = require("pg-connection-string");
+// const { parse } = require("pg-connection-string");
+
+// module.exports = ({ env }) => {
+//   const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+//   return {
+//     connection: {
+//       client: 'postgres',
+//       connection: {
+//         host,
+//         port,
+//         database,
+//         user,
+//         password,
+//         ssl: env.bool('DATABASE_SSL', false),
+//       },
+//     },
+//   }
+// };
+const path = require('path');
 
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-  console.log(host, port, database, user, password);
   return {
     connection: {
-      client: 'postgres',
+      client: 'sqlite',
       connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
-        ssl: env.bool('DATABASE_SSL', false),
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
       },
+      useNullAsDefault: true,
     },
   }
 };
+
